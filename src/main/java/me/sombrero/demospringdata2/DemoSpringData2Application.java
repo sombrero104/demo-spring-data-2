1e83b2c1dbe5d161608d0b6cf71344be2d171ef6
sombrero104 <sombrero.khy@gmail.com>
2021-03-04 22:00:35 +0900

    .

---
+++ src/main/java/me/sombrero/demospringdata2/DemoSpringData2Application.java
@@ -0,0 +1,13 @@
+package me.sombrero.demospringdata2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringData2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringData2Application.class, args);
+    }
+
+}
---
+++ src/test/java/me/sombrero/demospringdata2/DemoSpringData2ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.sombrero.demospringdata2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringData2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

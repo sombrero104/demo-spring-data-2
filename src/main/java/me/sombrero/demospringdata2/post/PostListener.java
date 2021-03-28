c5fbf992313ddb05bba3e63c185d97153b0884b3
sombrero104 <sombrero.khy@gmail.com>
2021-03-28 18:26:14 +0900

    .

--- src/main/java/me/sombrero/demospringdata2/post/PostListener.java
+++ src/main/java/me/sombrero/demospringdata2/post/PostListener.java
@@ -3,6 +3,12 @@ package me.sombrero.demospringdata2.post;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.EventListener;
 
+/**
+ * 이벤트 리스너를 만드는 두가지 방법.
+ * 1. ApplicationListener를 구현하여 onApplicationEvent() 메소드를 오버라이딩 한다. (이벤트 발생 시 실행할 코드를 작성하는 메소드.)
+ * 2. 이벤트 발생 시 실행할 코드를 작성하는 메소드에 @EventListener 애노테이션을 붙여준다.
+ * 리스너를 만든 후 빈으로 등록해줘야 한다. (여기서는 PostRepositoryTestConfig에서 빈으로 등록하고 있다.)
+ */
 public class PostListener { // implements ApplicationListener<PostPublishedEvent> {
 
     /**

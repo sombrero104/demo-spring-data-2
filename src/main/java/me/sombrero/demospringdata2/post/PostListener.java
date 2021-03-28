51b01755bfbc40ccc5978c502bf46e052f2a49fb
sombrero104 <sombrero.khy@gmail.com>
2021-03-28 17:42:53 +0900

    .

--- src/main/java/me/sombrero/demospringdata2/post/PostListener.java
+++ src/main/java/me/sombrero/demospringdata2/post/PostListener.java
@@ -11,7 +11,7 @@ public class PostListener implements ApplicationListener<PostPublishedEvent> {
     @Override
     public void onApplicationEvent(PostPublishedEvent event) {
         System.out.println("-----------------------------------");
-        System.out.println(event.getPost() + " is published!!");
+        System.out.println(event.getPost().getTitle() + " is published!!");
         System.out.println("-----------------------------------");
     }
 
